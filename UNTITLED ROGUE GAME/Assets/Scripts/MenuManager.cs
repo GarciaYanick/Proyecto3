c21663ac54536cbb131afbed704dad8d0d8740e4
iulia.todor.7e6 <iulia.todor.7e6@itb.cat>
2024-04-05 16:05:09 +0200

    Main Menu

---
+++ UNTITLED ROGUE GAME/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    private Animator animator;
+
+    public AnimationClip clip;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SceneChange(string sceneName)
+    {
+        
+    }
+    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadAnimationTransition()
+    {
+        StartCoroutine(LoadAnimation());
+    }
+
+    IEnumerator LoadAnimation()
+    {
+        animator.SetTrigger("StartGame");
+
+        yield return new WaitForSeconds(clip.length + 1);
+
+        SceneManager.LoadScene("MainScene");
+    }
+
+
+
+}
