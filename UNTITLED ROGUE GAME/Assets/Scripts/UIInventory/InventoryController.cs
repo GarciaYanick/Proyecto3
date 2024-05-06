4e7249e92c87725847719294807cd89ab080b02a
Yanick Garcia <yanick.garcia.7e6@itb.cat>
2024-05-06 16:57:47 +0200

    Inventario iniciado
    La letra i lo hace aparecer y desaparecer, trabajando en el drag and drop.

---
+++ UNTITLED ROGUE GAME/Assets/Scripts/UIInventory/InventoryController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private UIInventoryPage inventoryUI;
+
+    public int invSize = 8;
+
+    private void Start()
+    {
+        inventoryUI.InitializeInvUI(invSize);       
+    }
+
+    private void Update()
+    {
+       if (Input.GetKeyUp(KeyCode.I))
+        {
+            if(inventoryUI.isActiveAndEnabled == false)
+            {
+                inventoryUI.Show();
+            }
+            else
+            {
+                inventoryUI.Hide();
+            }
+        }
+    }
+}
