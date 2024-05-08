9a58b928e253da3f2355a214a228939fe4a9b64c
Oriol Gali <oriol.gali.7e6@itb.cat>
2024-05-08 16:50:04 +0200

    logica cofres
    faltan animaciones y instanciar el contenido

---
+++ UNTITLED ROGUE GAME/Assets/ChestLogic.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using UnityEngine;
+
+public class ChestLogic : MonoBehaviour
+{
+    private LevelLogic _levelLogic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _levelLogic = GameObject.Find("Level").GetComponent<LevelLogic>();
+        _levelLogic.OnEnemiesKilled += ActivateChest;
+    }
+
+    public void ActivateChest(object sender, EventArgs e )
+    {
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ( collision.CompareTag("Player"))
+        {
+            //animacion para abrir cofre e instanciacion del item recogible/ pantalla de item obtenido
+        }
+    }
+}
--- UNTITLED ROGUE GAME/Assets/Scripts/LevelLogic.cs
+++ UNTITLED ROGUE GAME/Assets/Scripts/LevelLogic.cs
@@ -1,13 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelLogic : MonoBehaviour
 {
+
+    [SerializeField] private List<GameObject> _enemies;
+    public EventHandler OnEnemiesKilled; 
     // Start is called before the first frame update
     void Start()
     {
+   
+    }
 
+    private void Update()
+    {
+        if (_enemies.Count <= 0)
+        {
+            OnEnemiesKilled?.Invoke(this, EventArgs.Empty);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -19,6 +31,5 @@ public class LevelLogic : MonoBehaviour
             GameManager.Instance.LevelChange();
             collision.gameObject.transform.position = new Vector2(0, 0);
         }
-
     }
 }
